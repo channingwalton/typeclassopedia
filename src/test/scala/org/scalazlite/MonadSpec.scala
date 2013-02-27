1b5d66b97c4e190654814ec806333744fad0249d
Channing Walton <channingwalton@mac.com>
2013-02-27 22:51:24 +0000

    another test

--- src/test/scala/org/scalazlite/MonadSpec.scala
+++ src/test/scala/org/scalazlite/MonadSpec.scala
@@ -8,8 +8,16 @@ class MonadSpec extends FlatSpec {
   "A monad" should "flatMap that thang" in {
     // I'll use >>= instead so we know scalaz-lite is being used
     val x: Option[Int] = Some(1)
-    val y = x >>= (x ⇒ Some(x + 1))
-    y === Some(2)
+    (x >>= (x ⇒ Some(x + 1))) === Some(2)
   }
 
+  it should "support new monads" in {
+    case class Blub[T](v: T)
+    implicit object BlubMonad extends Monad[Blub] {
+      def pure[A](a: ⇒ A) = Blub(a)
+      def flatMap[A, B](ma: Blub[A], f: A ⇒ Blub[B]) = f(ma.v)
+    }
+
+    (Blub(1) >>= (x ⇒ Blub(x + 1))) === Blub(2)
+  }
 }
