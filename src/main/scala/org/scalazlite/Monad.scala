180ab2710d155d6c30d5ba6f51e167ced1aa944f
Channing Walton <channingwalton@mac.com>
2013-03-01 22:21:39 +0000

    deleting duplicate

--- src/main/scala/org/scalazlite/Monad.scala
+++ src/main/scala/org/scalazlite/Monad.scala
@@ -4,7 +4,6 @@ package org.scalazlite
  *   The concept of a Monad
  */
 trait Monad[M[_]] extends Applicative[M] {
-  def pure[A](a: ⇒ A): M[A]
   def flatMap[A, B](ma: M[A], f: A ⇒ M[B]): M[B]
 }
 
@@ -19,10 +18,5 @@ trait Monads {
     def flatMap[B](f: T ⇒ M[B]): M[B] = implicitly[Monad[M]].flatMap(value, f)
     // lets add Haskell's 'bind' 
     def >>=[B](f: T ⇒ M[B]): M[B] = flatMap(f)
-  }
-
-  // wrap a simple type in a Monad M, if an implicit Monad[M] is available
-  implicit class MonadValueOps[T](value: T) {
-    def pure[M[_]](implicit M: Monad[M]): M[T] = M.pure(value)
   }
 }
