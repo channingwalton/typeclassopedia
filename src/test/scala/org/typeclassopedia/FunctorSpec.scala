5a8fe563234a8355dad6e8ad41d490ac8dac4b81
Channing Walton <channingwalton@mac.com>
2013-03-08 23:27:49 +0000

    laws for functor

--- src/test/scala/org/typeclassopedia/FunctorSpec.scala
+++ src/test/scala/org/typeclassopedia/FunctorSpec.scala
@@ -5,5 +5,10 @@ import Typeclassopedia._
 
 class FunctorSpec extends FlatSpec {
 
-  "A functor" should "map stuff" in assert(Blub(1).map(2*) === Blub(2))
+  val plus1 = (_: Int) + 1
+  val x2 = (_: Int) * 2
+
+  "A functor" should "map stuff" in assert(Blub(1).map(x2) === Blub(2))
+  it should "obey the identity law" in assert(Blub(1).map(identity) === Blub(1))
+  it should "obey the composition law" in assert(Blub(1).map(x2 compose plus1) === Blub(1).map(plus1).map(x2))
 }
