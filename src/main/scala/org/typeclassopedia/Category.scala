42a379995e856ba4c1163b9b1270720b324b8e63
Channing Walton <channingwalton@mac.com>
2013-05-01 08:02:17 +0100

    added Category

---
+++ src/main/scala/org/typeclassopedia/Category.scala
@@ -0,0 +1,18 @@
+package org.typeclassopedia
+
+/**
+ * A Category generalizes the notion of function composition to general morphisms.
+ *
+ * Recall that a type parameterised by two types can be used in infix notation.
+ * So the ~>[_, _] below can be used like this:  A ~> B which is the same as ~>[A, B].
+ * It is convenient to use the type in infix position to convey the idea of a morphism.
+ *
+ * Note also that Scalaz uses =>: rather than ~> but I thought I'd use what the TMR-Issue 13 - Typeclassopedia
+ * paper used.
+ */
+trait Category[~>[_, _]] {
+
+  def id[A]: A ~> A
+
+  def compose[A, B, C](f: B ~> C, g: A ~> B): A ~> C
+}
