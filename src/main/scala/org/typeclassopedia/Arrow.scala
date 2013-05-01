87e8f998824720da387765c74564addb8fce5aa4
Channing Walton <channingwalton@mac.com>
2013-05-01 20:19:58 +0100

    added Arrow and Arrow type class for Function1

---
+++ src/main/scala/org/typeclassopedia/Arrow.scala
@@ -0,0 +1,31 @@
+package org.typeclassopedia
+
+/**
+ * Arrow is explained in the Monad.Reader Issue 13 far better than I can.
+ * In particular, read page 52 which discusses the methods below.
+ * (The implementation of the methods in this trait are borrowed from scalaz.)
+ */
+trait Arrow[~>[_, _]] extends Category[~>] {
+
+  def arr[B, C](f: B => C): B ~> C
+
+  def first[B, C, D](f: (B ~> C)): (B, D) ~> (C, D)
+
+  def second[A, B, C](f: (A ~> B)): ((C, A) ~> (C, B)) = {
+    def swap[X, Y] = arr[(X, Y), (Y, X)] {
+      case (x, y) => (y, x)
+    }
+
+    compose(swap, compose(first[A, B, C](f), swap))
+  }
+
+  def ***[B, C, B2, C2](fbc: B ~> C, fbc2: B2 ~> C2): (B, B2) ~> (C, C2) =
+    compose(second[B2, C2, C](fbc2), first[B, C, B2](fbc))
+
+  def &&&[B, C, C2](fbc: B ~> C, fbc2: B ~> C2): B ~> (C, C2) =
+    compose(split(fbc, fbc2), arr((b: B) => (b, b)))
+
+  def split[A, B, C, D](f: A ~> B, g: C ~> D): ((A, C) ~> (B, D)) =
+    compose(second[C, D, B](g), first[A, B, C](f))
+
+}
--- src/main/scala/org/typeclassopedia/std/Implementations.scala
+++ src/main/scala/org/typeclassopedia/std/Implementations.scala
@@ -1,4 +1,4 @@
 package org.typeclassopedia
 package std
 
-trait Implementations extends Options with Lists with Monoids
+trait Implementations extends Options with Lists with Monoids with Function1Categories
