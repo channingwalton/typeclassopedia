3333b4bf09f4eff772be2cf34fa6700e99fb01df
Channing Walton <channingwalton@mac.com>
2013-06-04 08:16:53 +0100

    adding more ops to Traversable

--- src/main/scala/org/typeclassopedia/Traversable.scala
+++ src/main/scala/org/typeclassopedia/Traversable.scala
@@ -1,13 +1,10 @@
 package org.typeclassopedia
 
 trait Traversable[M[_]] extends Functor[M] with Foldable[M] {
-  /*
- * traverse ::Applicative f=>(a->fb)->ta->f(tb) 
- * sequenceA :: Applicative f⇒ t (f a) → f (t a)
- * mapM :: Monad m⇒ (a → m b) → t a → m (t b)
- * sequence ::Monadm=>t(ma)->m(ta)
- */
+
   def traverse[G[_] : Applicative, A, B](fa: M[A])(f: A ⇒ G[B]): G[M[B]]
+
+  def sequence[G[_] : Applicative, A](fga: M[G[A]]): G[M[A]] = traverse[G, G[A], A](fga)(identity)
 }
 
 /**
@@ -16,9 +13,12 @@ trait Traversable[M[_]] extends Functor[M] with Foldable[M] {
  */
 trait Traversables {
 
-  // enrich a value of M[T] with traversable operations 
   implicit class TraversableOps[M[_] : Traversable, T](value: M[T]) {
     def traverse[G[_] : Applicative, B](f: T ⇒ G[B]): G[M[B]] = implicitly[Traversable[M]].traverse(value)(f)
+  }
+
+  implicit class SequenceOps[M[_] : Traversable, G[_]: Applicative, T](value: M[G[T]]) {
+    def sequence: G[M[T]] = implicitly[Traversable[M]].sequence[G, T](value)
   }
 
 }
--- src/test/scala/org/typeclassopedia/TraversableSpec.scala
+++ src/test/scala/org/typeclassopedia/TraversableSpec.scala
@@ -5,7 +5,9 @@ import org.scalatest._
 
 class TraversableSpec extends FlatSpec {
 
-  "A Traversable" should "support sequence" in assert(1.some.traverse(v ⇒ List(v, v + 1)) === List(1.some, 2.some))
+  "A Traversable" should "support traverse" in assert(1.some.traverse(v ⇒ List(v, v + 1)) === List(1.some, 2.some))
+
+  it should "support sequence" in assert(List(1.some).sequence === List(1).some)
 
   it should "preserve order" in assert(List(1, 2, 3).traverse(Option(_)) === Some(List(1, 2, 3)))
 
@@ -18,4 +20,6 @@ class TraversableSpec extends FlatSpec {
     val s = List.range(0, 32 * 1024).traverse(x ⇒ x.some)
     assert(s.map(_.take(3)) === Some(List(0, 1, 2)))
   }
+
+
 }
