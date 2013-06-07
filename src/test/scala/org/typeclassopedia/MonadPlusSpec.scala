4380f62db46d8777370fc23f0f7711b3c99e50ca
Channing Walton <channingwalton@mac.com>
2013-06-07 17:51:24 +0100

    added MonadPlus laws tests which required >> added to Monad

--- src/test/scala/org/typeclassopedia/MonadPlusSpec.scala
+++ src/test/scala/org/typeclassopedia/MonadPlusSpec.scala
@@ -7,7 +7,7 @@ class MonadPlusSpec extends FlatSpec {
 
   val x: Option[Int] = Some(1)
 
-  "Monad Plus" should "mzero" in {
+  "Monad Plus" should "support mzero" in {
     assert(implicitly[MonadPlus[List]].mzero === Nil)
   }
 
@@ -18,4 +18,13 @@ class MonadPlusSpec extends FlatSpec {
     assert( (none[Int] mplus 2.some) === Some(2) )
     assert( (none[Int] mplus none[Int]) === None )
   }
+
+  it should "obey monad plus laws" in {
+    val mplus = implicitly[MonadPlus[List]]
+    val add = (i:Int) => i :: i :: Nil
+
+    assert((mplus.mzero[Int] >>= add) === mplus.mzero[Int])
+    assert( (List(1) >> Nil) === Nil)
+  }
+
 }
