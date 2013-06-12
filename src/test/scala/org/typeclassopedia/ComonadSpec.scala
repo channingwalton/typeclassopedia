542f1fba6205b5c3182d3d5f86a27831ee1730cd
Channing Walton <channingwalton@mac.com>
2013-06-12 07:55:08 +0100

    added comonad spec

--- src/test/scala/org/typeclassopedia/ComonadSpec.scala
+++ src/test/scala/org/typeclassopedia/ComonadSpec.scala
@@ -8,4 +8,8 @@ class ComonadSpec extends FlatSpec {
    "A comonad" should "duplicate Option" in { assert( Option(1).duplicate === Option(Option(1)) ) }
    it should "duplicate List" in { assert( List(1).duplicate === List(List(1)) ) }
    it should "duplicate Blub" in { assert( Blub(1).duplicate === Blub(Blub(1)) ) }
+
+  it should "extend Option" in { assert( Option(1).extend(_ ⇒ 2) === Option(2) ) }
+  it should "extend List" in { assert( List(1).extend(_.head + 1) === List(2) ) }
+  it should "extend Blub" in { assert( Blub(1).extend(_.v + 1) === Blub(2) ) }
  }
