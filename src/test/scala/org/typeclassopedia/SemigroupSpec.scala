283bb924b84acad2029c433a0896398831a91987
Channing Walton <channingwalton@mac.com>
2013-11-15 22:39:27 +0000

    fiddling

--- src/test/scala/org/typeclassopedia/SemigroupSpec.scala
+++ src/test/scala/org/typeclassopedia/SemigroupSpec.scala
@@ -2,11 +2,10 @@ package org.typeclassopedia
 
 import Typeclassopedia._
 import org.scalatest._
+import Matchers._
 
-class SemigroupSpec extends FlatSpec with MustMatchers {
-
+private class SemigroupSpec extends FlatSpec {
   "Append" should "append ints" in { (1 |+| 2) === 3 }
-
   it should "append options" in {
     (1.some |+| 2.some) === 3.some
     (1.some |+| None) === 1.some
