cbc9692c260efbbb71d2eda549f6351525ecb784
Channing Walton <channingwalton@mac.com>
2014-08-30 22:59:15 +0100

    added some docs

--- src/main/scala/org/typeclassopedia/transformers/OptionT.scala
+++ src/main/scala/org/typeclassopedia/transformers/OptionT.scala
@@ -2,16 +2,19 @@ package org.typeclassopedia.transformers
 
 import org.typeclassopedia._
 
-case class OptionT[F[_] : Monad, A](fa: F[Option[A]]) {
+/**
+ * The type OptionT[M[_], A] is a monad transformer that constructs an Option[A] inside the monad M.
+ */
+case class OptionT[M[_] : Monad, A](fa: M[Option[A]]) {
 
-  val monadF = implicitly[Monad[F]]
+  val monadF = implicitly[Monad[M]]
 
-  def map[B](f: A ⇒ B): OptionT[F, B] = OptionT(monadF.map(fa, (_: Option[A]).map(f)))
+  def map[B](f: A ⇒ B): OptionT[M, B] = OptionT(monadF.map(fa, (_: Option[A]).map(f)))
 
-  def flatMap[B](f: A => OptionT[F, B]): OptionT[F, B] = OptionT(monadF.flatMap(fa, (o: Option[A]) ⇒ o match {
+  def flatMap[B](f: A => OptionT[M, B]): OptionT[M, B] = OptionT(monadF.flatMap(fa, (o: Option[A]) ⇒ o match {
     case None ⇒ monadF.pure(Option.empty[B])
     case Some(x) ⇒ f(x).run
   }))
 
-  def run = fa
+  def run: M[Option[A]] = fa
 }
---
+++ src/main/scala/org/typeclassopedia/transformers/package.scala
@@ -0,0 +1,20 @@
+package org.typeclassopedia
+
+/**
+ * This package has some monad transformers.
+ *
+ * It is very common to have nested types like List[Option[A]],
+ * or IO[Option[A]]. It is inconvenient to work with values of these
+ * types directly with nested for-comprehensions or functions.
+ *
+ * A monad transformer combines two monads into a single monad that shares
+ * the behaviour of both.
+ *
+ * See the example implementations for specific examples.
+ *
+ * @see http://underscoreconsulting.com/blog/posts/2013/12/20/scalaz-monad-transformers.html
+ * @see http://eed3si9n.com/learning-scalaz/Monad+transformers.html
+ * @see http://en.wikibooks.org/wiki/Haskell/Monad_transformers
+ * @see http://www.haskell.org/haskellwiki/Monad_Transformers_Explained
+ */
+package object transformers
