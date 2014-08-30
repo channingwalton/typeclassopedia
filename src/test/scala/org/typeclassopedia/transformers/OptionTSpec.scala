09a926ef1163df9fcbc345eefa8fb5fd6e6f63a2
Channing Walton <channingwalton@mac.com>
2014-08-30 22:15:05 +0100

    refactoring

--- src/test/scala/org/typeclassopedia/transformers/OptionTSpec.scala
+++ src/test/scala/org/typeclassopedia/transformers/OptionTSpec.scala
@@ -2,13 +2,12 @@ package org.typeclassopedia.transformers
 
 import org.scalatest.{Matchers, FlatSpec}
 import org.typeclassopedia.Blub
-import org.typeclassopedia.Typeclassopedia.none
 
 class OptionTSpec extends FlatSpec with Matchers {
 
   "OptionT" should "map" in {
     OptionT(Blub(Option("hi"))).map(_ ⇒ 1) shouldEqual OptionT(Blub(Option(1)))
-    OptionT(Blub(none[String])).map(_ ⇒ 1) shouldEqual OptionT(Blub(none[Int]))
+    OptionT(Blub(Option.empty[String])).map(_ ⇒ 1) shouldEqual OptionT(Blub(Option.empty[Int]))
   }
 
   it should "flatMap" in {
