7e9de1353f2cd9ecf8d3a0c2563f59e356e2b183
Channing Walton <channingwalton@mac.com>
2014-08-31 21:27:07 +0100

    added a test

--- src/test/scala/org/typeclassopedia/transformers/OptionTSpec.scala
+++ src/test/scala/org/typeclassopedia/transformers/OptionTSpec.scala
@@ -14,4 +14,20 @@ class OptionTSpec extends FlatSpec with Matchers {
     val fm: OptionT[Blub, Int] = OptionT(Blub(Option(1)))
     OptionT(Blub(Option("hi"))).flatMap(_ ⇒ fm) shouldEqual fm
   }
+
+  it should "do the for-comprehension thing with all the options" in {
+    val r = for {
+      a ← OptionT(Blub(Option(1)))
+      b ← OptionT(Blub(Option(1)))
+    } yield a + b
+    r shouldEqual OptionT(Blub(Option(2)))
+  }
+
+  it should "do the for-comprehension thing with a none" in {
+    val r = for {
+      a ← OptionT(Blub(Option(1)))
+      b ← OptionT(Blub(Option.empty[Int]))
+    } yield a + b
+    r shouldEqual OptionT(Blub(Option.empty[Int]))
+  }
 }
